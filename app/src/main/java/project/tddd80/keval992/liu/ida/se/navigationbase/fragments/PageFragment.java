be245bdf9b0dcc3b256a5766c28ec99894badbc0
Kevin <somjai.kevin@gmail.com>
2015-08-24 12:15:49 +0200

    Added favorite button and toogling of favorite.

--- app/src/main/java/project/tddd80/keval992/liu/ida/se/navigationbase/fragments/PageFragment.java
+++ app/src/main/java/project/tddd80/keval992/liu/ida/se/navigationbase/fragments/PageFragment.java
@@ -6,11 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
 import project.tddd80.keval992.liu.ida.se.navigationbase.R;
 import project.tddd80.keval992.liu.ida.se.navigationbase.models.Page;
+import project.tddd80.keval992.liu.ida.se.navigationbase.network.HttpRequestTask;
+import project.tddd80.keval992.liu.ida.se.navigationbase.network.JSONFactory;
+import project.tddd80.keval992.liu.ida.se.navigationbase.network.JSONParser;
 
 /**
  * A simple {@link Fragment} subclass for displaying a pages information.
@@ -19,6 +25,7 @@ public class PageFragment extends Fragment implements View.OnClickListener {
 
     private Page page;
     private ImageView profileImage;
+    private Button favorite;
     private TextView name, type, location, information;
 
     public PageFragment() {
@@ -48,12 +55,57 @@ public class PageFragment extends Fragment implements View.OnClickListener {
         type.setText(page.getType());
         location = (TextView) linView.findViewById(R.id.location);
         location.setText(page.getLocation());
-        location.setOnClickListener(this);
+        // FOR MAPS
+        // location.setOnClickListener(this);
+        // location.setTextColor(getResources().getColor(R.color.linkColour));
+        favorite = (Button) linView.findViewById(R.id.favorite);
+        favorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendFavoriteRequest();
+            }
+        });
     }
 
     private void setInfoCard(View infoView, Page page) {
         information = (TextView) infoView.findViewById(R.id.information);
         information.setText(page.getInformation());
+    }
+
+    private void sendFavoriteRequest() {
+        String routing = "";
+        if (page.isHasUserFavorited()) {
+            routing = "delete";
+        } else {
+            routing = "new";
+        }
+        new HttpRequestTask(routing) {
+
+            @Override
+            protected void atPostExecute(JSONObject jsonObject) {
+                if (JSONParser.wasSuccessful(jsonObject)) {
+                    toogleFavoriteButton();
+                }
+            }
+        }.execute(JSONFactory.createFavoriteData(page.getId()));
+    }
+
+    private void toogleFavoriteButton() {
+        if (!page.isUserMember()) {
+            if (page.isHasUserFavorited()) {
+                favorite.setText("Remove from favorites");
+                favorite.setBackgroundColor(getResources().getColor(R.color.normalColor));
+                page.setHasUserFavorited(false);
+            } else {
+                favorite.setText("Add to favorites");
+                favorite.setBackgroundColor(getResources().getColor(R.color.toogleColor));
+                page.setHasUserFavorited(true);
+            }
+        } else {
+            favorite.setVisibility(View.GONE);
+            favorite.setEnabled(false);
+            favorite.setOnClickListener(null);
+        }
     }
 
     @Override
